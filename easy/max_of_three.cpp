49d24f83a7ffe32fc428019fdc2d9fcb302e4199
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-16 18:31:20 +0530

    Create max_of_three.cpp

---
+++ easy/max_of_three.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main() {
+   int a,b,c;
+   cin>>a>>b>>c;
+   vector<int>v={a,b,c};
+   sort(v.begin(),v.end());
+   cout<<v[2];
+
+    return 0;
+}
+/*
+output
+3 1 9
+9*/
