d2aa2b36071baa67e1f3f8edd7d96f07072f60e3
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-17 06:18:54 +0530

    Create preorder.cpp

---
+++ medium/preorder.cpp
@@ -0,0 +1,13 @@
+void pre(Node *root,vector<int>&v){
+    if(root!=NULL){
+        v.push_back(root->data);
+        pre(root->left,v);
+        
+        pre(root->right,v);
+    }
+}
+vector<int> preorder(Node *root){
+    vector<int>v;
+    pre(root,v);
+    return v;
+}
